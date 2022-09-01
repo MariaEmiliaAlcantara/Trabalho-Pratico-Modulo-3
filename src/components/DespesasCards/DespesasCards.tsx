c330b058ab29cd6a482ed50a1bc37ea66b072fec
MariaSchinoni <maria.schinoni10@gmail.com>
2022-09-01 16:53:52 -0300

    message for month and year without data

--- src/components/DespesasCards/DespesasCards.tsx
+++ src/components/DespesasCards/DespesasCards.tsx
@@ -1,12 +1,12 @@
-import { IApi } from "../../App";
+import { IApi } from "../../pages/Home/Home";
 import Card from "../Card/Card";
 
 const DespesasCards: React.FC<{ despesas: IApi[] }> = ({ despesas }) => {
   return (
     <div>
-      {despesas.map((despesa) => (
-        <Card key={despesa.id} despesa={despesa} />
-      ))}
+      {despesas.length > 0
+        ? despesas.map((despesa) => <Card key={despesa.id} despesa={despesa} />)
+        : "Não há despesas cadastradas referentes a este período"}
     </div>
   );
 };

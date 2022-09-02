43e56066cde3e5e416fb0120fd7075eb2ccb2134
MariaSchinoni <maria.schinoni10@gmail.com>
2022-09-02 14:59:39 -0300

    header material UI

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,21 @@
 import "./Header.css";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
-const Header = () => {
+export default function Header() {
   return (
-    <>
-      <div className="HeaderTop">Aplicativo de Despesas Mensais</div>
-    </>
+    <Box className="header" sx={{ flexGrow: 1 }}>
+      <AppBar color="transparent" position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Despesas pessoais
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
-};
-
-export default Header;
+}

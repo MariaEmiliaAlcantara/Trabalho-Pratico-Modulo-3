73cf0b82375af44299ad0e04837b34606b1b33e5
MariaSchinoni <maria.schinoni10@gmail.com>
2022-09-02 15:04:41 -0300

    total expenses material UI

--- src/components/DespesaTotal/DespesaTotal.tsx
+++ src/components/DespesaTotal/DespesaTotal.tsx
@@ -1,5 +1,18 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import "./DespesaTotal.css";
+
 const DespesaTotal: React.FC<{ despesaTotal: number }> = ({ despesaTotal }) => {
-  return <div>Despesa total: {Number(despesaTotal.toFixed(2))}</div>;
+  return (
+    <Card>
+      <CardContent className="despesaTotal">
+        <Typography>
+          Despesa total: {Number(despesaTotal.toFixed(2))}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default DespesaTotal;
